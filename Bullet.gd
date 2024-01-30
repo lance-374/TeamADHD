6fffe1c32eb5fbbb8f3ff8c100b5522466c89c03
Adam Chermouh <achermouh808@gmail.com>
2024-01-30 14:19:29 -0600

    Level 1:
    script need player1 and 2 data

---
+++ Bullet.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var speed = 750
+
+func _physics_process(delta):
+	position += transform.x * speed * delta
+
+func _on_Bullet_body_entered(body):
+	if body == 'player1':
+		body.queue_free()
+	queue_free()
+	
+
