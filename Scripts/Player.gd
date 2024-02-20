cfca3844694dc00f7898e15c1c3c4c1797ffc775
lance-374 <58232106+lance-374@users.noreply.github.com>
2024-02-20 14:41:45 -0600

    fixed a bunch of stuff with gravity and animations

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,6 +5,7 @@ const FRICTION = 2000
 const ACCEL = 1200
 
 var aim_input = Vector2.ZERO
+var gravity_flipped = false
 @export var controller_id = "0"
 @export var jump_velocity = -600.0
 @export var gravity = 1960
@@ -12,8 +13,15 @@ var aim_input = Vector2.ZERO
 
 @onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var muzzle = $Muzzle
-@onready var Bullet = preload("res://Objects/bullet.tscn") # Will load when parsing the script.
+@onready var Bullet = preload("res://Objects/bullet.tscn") # Will load when parsing the script
+@onready var healthbar = $HealthBar
+@onready var collision_shape_2d = $CollisionShape2D
 var facing_left = false
+
+func _ready():
+	if self == null:
+		return
+	healthbar.init_health(health)
 
 func _physics_process(delta):
 	if self == null:
@@ -32,10 +40,28 @@ func _physics_process(delta):
 	if aim_input.x > 0:
 		facing_left = false
 	handle_shoot()
-	apply_accel(input_axis, delta)
+	apply_acceleration(input_axis, delta)
 	apply_friction(input_axis, delta)
 	update_animation(input_axis)
 	move_and_slide()
+	
+#	update positions of everything TODO, improve this using PositionNode2D
+	if facing_left and gravity_flipped:
+		collision_shape_2d.position = Vector2(14,-40)
+		healthbar.position = Vector2(-20, 12)
+		muzzle.position = Vector2(-20, -12)
+	elif not facing_left and gravity_flipped:
+		collision_shape_2d.position = Vector2(0,-40)
+		healthbar.position = Vector2(-32, 12)
+		muzzle.position = Vector2(26, -12)
+	elif facing_left and not gravity_flipped:
+		collision_shape_2d.position = Vector2(14,2)
+		healthbar.position = Vector2(-20, -52)
+		muzzle.position = Vector2(-20, -24)
+	elif not facing_left and not gravity_flipped:
+		collision_shape_2d.position = Vector2(0,2)
+		healthbar.position = Vector2(-32, -52)
+		muzzle.position = Vector2(26, -24)
 
 
 func apply_gravity(delta):
@@ -45,7 +71,7 @@ func apply_gravity(delta):
 func reverse_gravity():
 	gravity *= -1
 	jump_velocity *= -1
-
+	gravity_flipped = gravity < 0
 
 func handle_jump():
 	var jump = "jump" + controller_id
@@ -71,14 +97,11 @@ func handle_shoot():
 	var up = "up" + controller_id
 	var down = "down" + controller_id
 	if Input.is_action_just_pressed("shoot" + controller_id):
-		$BulletSound.play()
 		var b = Bullet.instantiate()
 		get_tree().root.add_child(b)
 		if facing_left and not Input.is_action_pressed(up) and not Input.is_action_pressed(down):
-			muzzle.position = Vector2(-18, -63)
 			b.set_speed(Vector2(-1, 0))
 		elif not facing_left and not Input.is_action_pressed(up) and not Input.is_action_pressed(down):
-			muzzle.position = Vector2(28, -63)
 			b.set_speed(Vector2(1, 0))
 		if Input.is_action_pressed(up):
 			b.set_speed(Vector2(0, -1))
@@ -86,34 +109,27 @@ func handle_shoot():
 			b.set_speed(Vector2(0, 1))
 		if aim_input.x != 0 or aim_input.y != 0:
 			b.set_speed(aim_input)
-		b.transform = $Muzzle.global_transform
+		b.transform = muzzle.global_transform
 
 func apply_friction(input_axis, delta):
 	if input_axis == 0:
 		velocity.x = move_toward(velocity.x, 0, FRICTION * delta)
 
-func apply_accel(input_axis, delta):
+func apply_acceleration(input_axis, delta):
 	if input_axis:
 		velocity.x = move_toward(velocity.x, SPEED * input_axis, ACCEL * delta)
 
 func update_animation(input_axis):
 	animated_sprite_2d.flip_h = facing_left
-	animated_sprite_2d.flip_v = gravity < 0
+	animated_sprite_2d.flip_v = gravity_flipped
 	if input_axis != 0:
 		animated_sprite_2d.play("run")
 	else:
 		animated_sprite_2d.play("idle")
-	
-	if not is_on_floor():
+	if not is_on_floor() and gravity > 0:
 		animated_sprite_2d.play("jump")
-
-#HEALTH BAR
-@onready var healthbar = $HealthBar
-
-func _ready():
-	if self == null:
-		return
-	healthbar.init_health(health)
+	elif not is_on_ceiling and gravity < 0:
+		animated_sprite_2d.play("jump")
 
 func update_health(h):
 	health = h
@@ -123,3 +139,4 @@ func update_health(h):
 
 func death():
 	queue_free()
+	#TODO add death animation here
